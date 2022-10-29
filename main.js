6070fe0762badd59c44033cd94ed07249f50c7a9
Md Shajjad Hossain Shazib <86253278+SHShazib@users.noreply.github.com>
2022-10-29 20:48:46 +0600

    updated

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// variables
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function (){
+    navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+}
